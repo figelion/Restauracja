42e98c7c15cee80df23a2b8dc64c14d8cb8a1a5d
Adam Bugaj <adam.m.bugaj@gmail.com>
2019-01-08 11:20:18 +0100

    users - fix(admin problem) + register view

--- restauracja/restauracja/urls.py
+++ restauracja/restauracja/urls.py
@@ -23,5 +23,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('menu/', include('menu.urls')),
-    path('user/', include('menu.urls')),
+    path('user/', include('users.urls')),
 ]
--- restauracja/users/urls.py
+++ restauracja/users/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 app_name = 'users'
-urlpatterns = {
-    path('', views.index, name='index'),
+urlpatterns = [
     path('signup/', views.register, name='register'),
-}
+]
