6b7f4491aaa39bf3c2d64a633998a2b4c92ccc84
Adam Bugaj <adam.m.bugaj@gmail.com>
2019-01-09 14:14:53 +0100

    user - login,logout,profile + links to other pages

--- restauracja/users/views.py
+++ restauracja/users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm, UserUpdateForm
+
 # Create your views here.
 
 
@@ -10,8 +12,26 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('menu:places')
+            messages.success(request, f'Your account has been created! You are now able to log in!')
+            return redirect('uesr:login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form':form})
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST,
+                                instance=request.user)
+        if u_form.is_valid():
+            u_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('users:profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+
+
+    context = {
+        'u_form': u_form
+    }
+    return render(request, 'users/profile.html', context)
